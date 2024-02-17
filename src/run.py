8b9b50a8005cd1b373c6c02807593c372b29edfd
Harsheys21 <97200758+Harsheys21@users.noreply.github.com>
2024-02-16 21:51:40 -0800

    Delete src directory

--- src/run.py
+++
@@ -1,5 +0,0 @@
-from gui import TicTacToeGUI
-from player import RandomPlayer, UserInputPlayer, UserWebcamPlayer
-
-if __name__ == '__main__':
-    TicTacToeGUI(RandomPlayer(), UserWebcamPlayer()).run()
