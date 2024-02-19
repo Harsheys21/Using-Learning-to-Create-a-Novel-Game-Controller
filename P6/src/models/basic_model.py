f16c8699196c9cf46d6391bcdbc749bcc718c2ea
cassidy aydin <caaydin@ucsc.edu>
2024-02-19 00:07:56 -0800

    part 6

--- P6/src/models/basic_model.py
+++ P6/src/models/basic_model.py
@@ -9,12 +9,15 @@ class BasicModel(Model):
         self.model.add(Rescaling(1./255, input_shape=input_shape))
         
         self.model.add(layers.Conv2D(4, (3, 3), activation='relu'))
+        self.model.add(layers.Dropout(0.1, noise_shape=None, seed=10))
         self.model.add(layers.MaxPooling2D((2, 2)))
         
         self.model.add(layers.Conv2D(8, (3, 3), activation='relu'))
+        self.model.add(layers.Dropout(0.1, noise_shape=None, seed=10))
         self.model.add(layers.MaxPooling2D((2, 2)))
         
         self.model.add(layers.Conv2D(16, (3, 3), activation='relu'))
+        self.model.add(layers.Dropout(0.1, noise_shape=None, seed=10))
         self.model.add(layers.MaxPooling2D((2, 2)))
         
         self.model.add(layers.Flatten())
